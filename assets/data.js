b28fd54c3e0d79c141e306733d30a5df7d0d8f9b
GeorgeKopf5 <gk3946@princeton.edu>
2026-09-04 23:41:03 -0400

    Add image of tracked swimming gait path

--- assets/data.js
+++ assets/data.js
@@ -74,9 +74,9 @@ window.PORTFOLIO = {
       gallery: [
         { src: 'images/flying-fish/FishCADPic1.png', caption: 'Solidworks CAD model of pectoral fin deployment and finning mechanism.' },
         { src: 'images/flying-fish/FishClosed.jpg', caption: 'Folding wing mechanism in the stowed swimming configuration.' },
+        { src: 'images/flying-fish/FishTracker.jpg', caption: 'Tracked swimming gait path at 2Hz speed.' },
         { src: 'images/flying-fish/FullCircuit.jpg', caption: 'Full circuit schematics for PCB and hard disconnect switch using magnetic-sensitive reed switch and MOSFET.' },
         { src: 'images/flying-fish/PCB_Good.png', caption: 'PCB Gerber rendering, includes feedback moderated voltage regulator and motor driver circuitry.' },
-        { src: 'media/flying-fish/4.jpg', caption: 'Launch trial breaking the surface in the test tank.' },
       ],
       video: { src: 'media/flying-fish/demo.mp4', caption: 'Various Swimming Trials' },
     },
