e6b21d517147da637dfe95cef43781e7e9d18c59
GeorgeKopf5 <gk3946@princeton.edu>
2026-09-07 14:00:39 -0400

    Update data.js

--- assets/data.js
+++ assets/data.js
@@ -79,8 +79,17 @@ window.PORTFOLIO = {
         { src: 'images/flying-fish/FishTracker.jpg', caption: 'Tracked swimming gait path at 2Hz speed.' },
       ],
 
-      video: { src: 'media/croppeddry.mp4', caption: 'Dry actuation of pectoral fins.' },  
-    },
+      videos: [
+  {
+    src: 'media/croppeddry.mp4',
+    caption: 'Dry actuation of pectoral fins.',
+  },
+  {
+    src: 'media/flying-fish/swim_trial.mp4',
+    caption: 'Tetherless swimming trial at 2 Hz.',
+    poster: 'images/flying-fish/FishTracker.jpg', // Optional: customize preview image per video
+  },
+],
     
     {
       id: 'search-rescue-robot',
