d56f8ddd6cd410d929aad3226fa733ed1c141a43
GeorgeKopf5 <gk3946@princeton.edu>
2026-09-07 14:39:39 -0400

    Comment out poster property in video data
    Comment out the poster property for video objects.

--- assets/data.js
+++ assets/data.js
@@ -83,12 +83,12 @@ window.PORTFOLIO = {
           {
            src: 'media/flying-fish/swimmingBar.mp4',
            caption: 'My initial versions of the RMO had no pectoral fins but could swim effectively with the aid of a buoyant rod.',
-           poster: 'images/flying-fish/FishBody.jpg', // Optional: customize preview image per video
+           //poster: 'images/flying-fish/FishBody.jpg', // Optional: customize preview image per video
           },
           {
            src: 'media/flying-fish/Lone Swimmer Clip.mp4',
            caption: 'Adjusting weight distribution and buoyancy helped the RMO swim freely on its own, although roll stability could use some improvements. ',
-           poster: 'images/flying-fish/FishBody.jpg', // Optional: customize preview image per video
+           //poster: 'images/flying-fish/FishBody.jpg', // Optional: customize preview image per video
           },
           {
            src: 'media/flying-fish/PrototypeI.mp4',
